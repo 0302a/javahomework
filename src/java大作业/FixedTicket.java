23cd42cae5ac505cc8b911b3e34e4f3875aff250
Shen <1091117251@qq.com>
2024-05-21 23:15:26 +0800

    第二次

---
+++ src/java大作业/FixedTicket.java
@@ -0,0 +1,8 @@
+package java大作业;
+
+public class FixedTicket extends Price{
+	 @Override
+	    public double getprice(Double distance) {
+	        return 0;
+	    }
+}
---
+++ src/java大作业/Price.java
@@ -0,0 +1,6 @@
+package java大作业;
+
+public abstract class Price {
+	  private double distance;
+      public abstract double getprice(Double distance);
+}
